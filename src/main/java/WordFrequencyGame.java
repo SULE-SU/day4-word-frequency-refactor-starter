0681b4084afd4bc8fc0f6891e20a156b4cf03068
SULE <sule1@oocl.com>
2025-09-04 15:50:02 +0800

    refactor：inline getlistMap

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -39,7 +39,19 @@ public class WordFrequencyGame {
             inputList.add(input);
         }
         //get the map for the next step of sizing the same word
-        Map<String, List<Input>> map = getListMap(inputList);
+        Map<String, List<Input>> map1 = new HashMap<>();
+        for (Input input1 : inputList) {
+//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
+            if (!map1.containsKey(input1.getValue())) {
+                ArrayList arr = new ArrayList<>();
+                arr.add(input1);
+                map1.put(input1.getValue(), arr);
+            } else {
+                map1.get(input1.getValue()).add(input1);
+            }
+        }
+
+        Map<String, List<Input>> map = map1;
 
         List<Input> frequencies = new ArrayList<>();
         for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
@@ -47,22 +59,6 @@ public class WordFrequencyGame {
             frequencies.add(input);
         }
         return frequencies;
-    }
-
-    private Map<String, List<Input>> getListMap(List<Input> inputList) {
-        Map<String, List<Input>> map = new HashMap<>();
-        for (Input input : inputList) {
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input.getValue())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(input);
-                map.put(input.getValue(), arr);
-            } else {
-                map.get(input.getValue()).add(input);
-            }
-        }
-
-        return map;
     }
 
 
