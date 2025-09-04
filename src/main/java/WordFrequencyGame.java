94c69b09aa507ac7844fc30bf92efc0c9d8a7163
SULE <sule1@oocl.com>
2025-09-04 20:42:09 +0800

    refactor：Extract composeOutput

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -15,11 +15,10 @@ public class WordFrequencyGame {
         try {
             List<WordFrequency> frequencies = countFrequencies(words);
             frequencies.sort(Comparator.comparingInt(WordFrequency::count).reversed());
+            return composeOutput(frequencies);
         } catch (Exception e) {
             return "Calculate Error";
         }
-
-        return inputStr;
     }
 
     // 用Stream直接计数，简化算法
@@ -31,25 +30,11 @@ public class WordFrequencyGame {
                 .collect(Collectors.toList());
     }
 
-    private static Map<String, List<String>> groupSameWords(String[] words) {
-        List<String> inputList = new ArrayList<>();
-        for (String s : words) {
-            inputList.add(s);
-        }
-        //get the map for the next step of sizing the same word
-        Map<String, List<String>> map = new HashMap<>();
-        for (String input1 : inputList) {
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input1)) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(input1);
-                map.put(input1, arr);
-            } else {
-                map.get(input1).add(input1);
-            }
-        }
-
-        return map;
+    // 抽取输出拼接方法
+    private String composeOutput(List<WordFrequency> frequencies) {
+        return frequencies.stream()
+                .map(wf -> wf.word() + " " + wf.count())
+                .collect(Collectors.joining("\n"));
     }
 
 
